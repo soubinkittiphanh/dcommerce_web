24ee322b8d486cfa5d76ea304ad27417440bc756
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2024-12-22 21:19:04 +0700

    demo

--- common/api.js
+++ common/api.js
@@ -1,18 +1,18 @@
 
 
 export const hostName = () => {
-  const baseURL = 'http://150.95.31.23:8015' // *** THE CORNERS
+  const baseURL = 'http://150.95.31.23:8014' // *** THE CORNERS
   // const baseURL = 'http://localhost:8888' // ***Demo 1*** 
   return baseURL;
 }
 export const mainCompanyInfo = () => {
   const info = {
-    name: 'THE CORNERS',
-    tel: '020 2698 3331',
-    whatsapp: '+8562023378899',
+    name: 'THE DCOMMERCE DEMO',
+    tel: '02099822489',
+    whatsapp: '+8562099822489',
     imageUrl: '',
     imageName: '',
-    address: '3 ແຍກອາໄພ່ LPB',
+    address: 'SAPHANTHONG ONLINE',
     env: ''
   }
   return info;
