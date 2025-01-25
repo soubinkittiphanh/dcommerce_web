bcb39dd14f2290e1a5c3685e4f4d5e403b6b3020
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2025-01-25 10:05:07 +0700

    onphet

--- common/api.js
+++ common/api.js
@@ -3,7 +3,8 @@
 export const hostName = () => {
   // const baseURL = 'http://150.95.31.23:8015' // *** THE CORNERS
   // const baseURL = 'http://150.95.31.23:8017' //  *** PHAIVANH PHARMACY
-  const baseURL = 'http://150.95.31.23:8018' //  *** BMART
+  // const baseURL = 'http://150.95.31.23:8018' //  *** BMART
+  const baseURL = 'http://150.95.31.23:8019' //  *** ONPHET
   // const baseURL = 'http://150.95.31.23:8014' //  *** DEMO1
   // const baseURL = 'http://localhost:8888' // ***Demo 1*** 
   return baseURL;
@@ -28,7 +29,7 @@ export const mainCompanyInfo = () => {
   //   env: ''
   // }
   const info = {
-    name: 'BMART',
+    name: 'ONPHET',
     tel: '020 9999 9999',
     whatsapp: '+85620 9999 9999',
     imageUrl: '',
