78bb004e5dccda4c62e47f1853e8d5c47307d079
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2025-03-13 21:18:05 +0700

    demo

--- common/api.js
+++ common/api.js
@@ -17,15 +17,18 @@ export const mainCompanyInfoV1 = (store) => {
 
 export const mainCompanyInfo = (store) => {
 
-  // const info = {
-  //   name: 'THE DCOMMERCE DEMO',
-  //   tel: '02099822489',
-  //   whatsapp: '+8562099822489',
-  //   imageUrl: '',
-  //   imageName: '',
-  //   address: 'SAPHANTHONG ONLINE',
-  //   env: ''
-  // }
+  const info = {
+    name: 'THE DCOMMERCE DEMO',
+    tel: '02099822489',
+    whatsapp: '+8562099822489',
+    imageUrl: '',
+    imageName: '',
+    address: 'SAPHANTHONG ONLINE',
+    ticketLogo: 'BWLOGO_PHAIVANH.jpeg',
+    companyLogo: 'company_logo_phaivanh.jpeg',
+    dcLogo: 'Dcommerce-Logo-phaivanh.png',
+    env: ''
+  }
   // const info = {
   //   name: 'ຮ້ານຂາຍຢາ ໄພວັນ ຟາມາຊີ',
   //   tel: '020 55 627 486',
@@ -76,17 +79,17 @@ export const mainCompanyInfo = (store) => {
   //   dcLogo: 'Dcommerce-Logo_DC.png',
   //   env: ''
   // }
-  const info = {
-    name: 'BMART',
-    tel: '020 2698 3331',
-    whatsapp: '+8562023378899',
-    imageUrl: '',
-    imageName: '',
-    address: '3 ແຍກອາໄພ່ LPB',
-    ticketLogo: 'BWLOGO_BMART.png',
-    companyLogo: 'company_logo_BMART.jpeg',
-    dcLogo: 'Dcommerce-Logo_DC.png',
-    env: ''
-  }
+  // const info = {
+  //   name: 'BMART',
+  //   tel: '020 2698 3331',
+  //   whatsapp: '+8562023378899',
+  //   imageUrl: '',
+  //   imageName: '',
+  //   address: '3 ແຍກອາໄພ່ LPB',
+  //   ticketLogo: 'BWLOGO_BMART.png',
+  //   companyLogo: 'company_logo_BMART.jpeg',
+  //   dcLogo: 'Dcommerce-Logo_DC.png',
+  //   env: ''
+  // }
   return info;
 }
