2da172ed2adf80f48c97d2750da525bc206b59ce
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2025-03-13 21:16:42 +0700

    demo

--- common/api.js
+++ common/api.js
@@ -4,9 +4,9 @@
 export const hostName = () => {
   // const baseURL = 'http://150.95.31.23:8015' // *** THE CORNERS
   // const baseURL = 'http://150.95.31.23:8017' //  *** PHAIVANH PHARMACY
-  const baseURL = 'http://150.95.31.23:8018' //  *** BMART
+  // const baseURL = 'http://150.95.31.23:8018' //  *** BMART
   // const baseURL = 'http://150.95.31.23:8019' //  *** TP AUTO PART
-  // const baseURL = 'http://150.95.31.23:8014' //  *** DEMO1
+  const baseURL = 'http://150.95.31.23:8014' //  *** DEMO1
   // const baseURL = 'http://localhost:8888' // ***Demo 1*** 
   return baseURL;
 }
