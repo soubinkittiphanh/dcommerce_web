93937f0950fdd9fad4863fe73c0293209c7e32b1
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2026-03-13 12:30:06 +0700

    UPDATE VERSION 42

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -4,7 +4,6 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from '../layouts/error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '../assets/css/mycss.css'
 
@@ -15,8 +14,6 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
 import '../node_modules/vue2-datepicker/index.css'
 
 import '../node_modules/vue-virtual-scroller/dist/vue-virtual-scroller.css'
-
-import '../node_modules/vuetify/dist/vuetify.css'
 
 import _d43c67dc from '../layouts/comingSoon.vue'
 import _6f6c098b from '../layouts/default.vue'
@@ -64,7 +61,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -210,10 +207,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
